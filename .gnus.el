bc49a968fe490340e69c2d6444e4743141ca3ae5
liam quinlan <>
2018-07-07 01:49:33 -0400

    gnus minimal

---
+++ .gnus.el
@@ -0,0 +1,36 @@
+
+;;; Code:
+
+
+(setq  user-mail-address	                "liamkquinlan@gmail.com"
+       user-full-name                           "liam" )
+
+
+(setq gnus-select-method                        '(nntp "news.gnus.org" ) )
+ 
+;;(add-to-list 'gnus-secondary-select-methods     '(nntp "news.gnus.org" ) )
+
+
+;;; .gnus.el ends here
+
+
+
+
+
+
+
+
+
+
+
+
+
+; ;; (setq gnus-select-method
+; ;;       '(nnimap "gmail"
+; ;; 	       (nnimap-address "imap.gmail.com")  ; it could also be imap.googlemail.com if that's your server.
+; ;; 	       (nnimap-server-port "imaps")
+; ;; 	       (nnimap-stream ssl)))
+; 
+; ;; (setq smtpmail-smtp-server "smtp.gmail.com"
+; ;;       smtpmail-smtp-service 587
+; ;;       gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
