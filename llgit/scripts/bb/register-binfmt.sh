16c323447d9db3f9e76d9bb3cd568ea49dacce7e
lk <liamkquinlan@gmail.com>
2020-10-31 11:58:48 -0400

    dplacid wsl (cleanup)

--- llgit/scripts/bb/register-binfmt.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-sudo sh -c "echo ':llvm:M::BC::/usr/local/bin/lli:' > /proc/sys/fs/binfmt_misc/register"
--- llgit/scripts/bb/targets-filtered.sh
+++
@@ -1,1 +0,0 @@
-ninja -t targets | grep -vi 'test\|check\|[.]a:\|[.]so[0-9.]*:\|install\|cache\|package'
